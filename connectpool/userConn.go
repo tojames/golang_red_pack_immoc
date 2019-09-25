de55f685e25916bfc8b9f7d7396757cf7d3b18ff
lxj <14889383@qq.com>
2019-09-25 18:22:52 +0800

    update connect pool

---
+++ connectpool/userConn.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"golang/connectpool/pool"
+)
+
+func main() {
+
+	ctx := context.Background()
+	config := &pool.Config{
+		MaxConn: 2,
+		MaxIdle: 1,
+	}
+	conn := pool.Prepare(ctx, config)
+	if _, err := conn.New(ctx); err != nil {
+		return
+	}
+	go conn.Release(ctx)
+	if _, err := conn.New(ctx); err != nil {
+		return
+	}
+	go conn.Release(ctx)
+	if _, err := conn.New(ctx); err != nil {
+		return
+	}
+	go conn.Release(ctx)
+	if _, err := conn.New(ctx); err != nil {
+		return
+	}
+	go conn.Release(ctx)
+	if _, err := conn.New(ctx); err != nil {
+		return
+	}
+}
