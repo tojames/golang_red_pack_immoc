2511b0975a9604119b7566cfdaf29fbb93762e45
lxj <14889383@qq.com>
2019-11-05 17:27:48 +0800

    context_example

--- context_example/main.go
+++ context_example/main.go
@@ -27,3 +27,56 @@ func handle(ctx context.Context, duration time.Duration) {
 		fmt.Println("process request with", duration)
 	}
 }
+
+func inc(a int) int {
+	res := a + 1
+	time.Sleep(1 * time.Second)
+	return res
+}
+
+func Add(ctx context.Context, a, b int) (res int) {
+	for i := 0; i < a; i ++ {
+		res = inc(res)
+		select {
+		case <- ctx.Done():
+			return -1
+		default:
+
+		}
+	}
+
+	for i := 0; i < b; i ++ {
+		res = inc(res)
+		select {
+		case <-ctx.Done():
+			return -1
+		default:
+		}
+	}
+	return
+}
+
+func main1() {
+	{
+		a := 1
+		b := 2
+		timeout := 2 * time.Second
+		ctx, _ := context.WithTimeout(context.Background(), timeout)
+
+		res := Add(ctx, 1, 2)
+		fmt.Printf("Compute: %d + %d, result: %d\n", a, b, res)
+	}
+
+	{
+		// 手动取消
+		a := 1
+		b := 2
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			time.Sleep(2 * time.Second)
+			cancel() // 在调用处主动取消
+		}()
+		res := Add(ctx, 1, 2)
+		fmt.Printf("Compute: %d+%d, result: %d\n", a, b, res)
+	}
+}
